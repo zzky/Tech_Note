20665fd02d2b82b360bdce19eeffaaad141d82c9
zzky <zzky134@163.com>
2020-08-24 00:03:09 +0800

    Update FileProcess.java

--- FileProcess.java
+++ FileProcess.java
@@ -20,29 +20,13 @@ public class FileProcess {
             Map<String, Integer> map = new HashMap<>();
             int count = 1;
             while ((line = bufferedReader.readLine()) != null) {
-                //TODO stage1-Notes_1-删除空行和字符小于1的行。
-                /*if (line.trim().length() > 1) {
-                    list.add(line);
-                    bufferedWriter.write(line + "\t\n");
-                    bufferedWriter.flush();
-                }*/
-                //TODO stage2-Notes_2-删除所有行里面带有数字的,
-                //保留包含大小写字符的
-                // 并把行后面的换行符空格等空白字符给去掉
-                /*if (!line.matches(".*\\d.*") && line.matches(".*[a-zA-Z].*")) {
-                    list.add(line);
-                    bufferedWriter.write(line.trim() + "\n");
-                    bufferedWriter.flush();
-                }*/
-                //System.out.println(list.size());
-                //TODO stage3-对所有行排序并按照倒序排列  <entity> <num>
-                //全大写，用于去重
+     
                 line = line.toUpperCase();
-                //去除网址数据
+         
                 if (line.matches(".*WWW\\..*")) {
                     continue;
                 }
-                //去除结尾冒号
+ 
                 if (line.endsWith(":")) {
                     line = line.substring(0, line.length() - 1);
                 }
